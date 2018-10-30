b14ca75e0ce543a5ce762c1e8178fa3e3c889487
Alberto <14043560+wesbluemarine@users.noreply.github.com>
2018-10-30 20:57:33 +0100

    Update screencast.sh

--- scripts/screencast.sh
+++ scripts/screencast.sh
@@ -1,3 +1,2 @@
-ffmpeg -f x11grab -r 30 -s 1920x1080 -i $DISPLAY -vcodec libx264 -preset ultrafast -threads 0 output.mkv
-
+ffmpeg -y -f -i -r 60 -f x11grab -s 1920x1080 -i {$DISPLAY} -c:v libx264rgb -crf 0 -preset:v ultrafast -c:a pcm_s16le -af aresample=async=1:first_pts=0 screencast.mk
 
