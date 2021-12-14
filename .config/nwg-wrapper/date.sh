861679febfdfa27a86c4f77523e8ec7d741b2c70
Alberto <14043560+wesbluemarine@users.noreply.github.com>
2021-12-15 00:58:27 +0100

    Overlay shrink
    Day abbreviation

--- .config/nwg-wrapper/date.sh
+++ .config/nwg-wrapper/date.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-time=$(LC_ALL=C TZ='Europe/Rome' date '+%H:%M %A ')
+time=$(LC_ALL=C TZ='Europe/Rome' date '+%H:%M %a ')
 date=$(date '+%d %B %Y')
 echo '<span size="20000" face="segoeui" weight="light" foreground="#FFFFFF">'  $time'</span><span size="10000" face="segoeui" weight="light" foreground="#FFFFFF">' 
 echo $date'</span>'
